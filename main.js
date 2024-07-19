4443a8d3b1fa5f13947a4eba10f513d05275a6b8
Jopiol <you@example.com>
2024-07-19 19:42:34 +0400

    Fixing click-spam and other little bugs

--- main.js
+++ main.js
@@ -9,21 +9,26 @@ const introduction = document.getElementById('introduction');
 const click = document.getElementById('click');
 
 function startFunction() {
+    //getting random time to make screen green
     const delay = Math.floor(Math.random() * 10000) + 1000;
+    //iterating over classes to change content
     introduction.classList.remove('active');
-
-  startButton.className = 'active';
+    startButton.className = 'active';
+    square.className = 'restrict';
+    
   setTimeout(() => {
     //defining custom color green with rgb
     const r = 36;
     const g = 142;
     const b = 68;
+    //iterating over classes to change content
+    square.classList.remove = 'restrict';
     square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     introduction.className = 'active';
     click.classList.remove('active');
     square.className = 'active';
-    startTime = performance.now(); // Record the start time
-
+     // Record the start time
+    startTime = performance.now();
     // Update the screen click event
     square.addEventListener('click', handleScreenClick, { once: true });
   }, delay);
@@ -34,17 +39,21 @@ function handleScreenClick() {
     const r = 29;
     const g = 29;
     const b = 148;
+    //end timer
     endTime = performance.now();
+    //iterating over classes to change content
     reloadButton.classList.remove('active');
     click.className = 'active';
     result.classList.remove('active');
-    const elapsedTime = endTime - startTime; // Calculate the elapsed time
+    // Calculate the elapsed time
+    const elapsedTime = endTime - startTime;
     square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     const finalResult = Math.round(elapsedTime);
     result.innerHTML = `Elapsed Time: ${finalResult} ms`
 }
 // Event listener for the start button
 startButton.addEventListener('click', startFunction);
+// Event listener for the reload button
 reloadButton.addEventListener('click', () => {
     location.reload();
 });
